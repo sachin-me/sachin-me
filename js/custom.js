948fb7c2ed1c75f2433094adf4d61da119c5f63c
sachin-me <sachinkumartiwari363@gmail.com>
2018-06-29 13:26:46 +0530

    This is my Portfolio

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//    var elems = document.querySelectorAll('.parallax');
+//    var instances = M.Parallax.init(elems, options);
+//  });
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
